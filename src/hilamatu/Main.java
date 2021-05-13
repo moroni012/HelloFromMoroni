ed4118e305f4fccabc30d8c2fffe85eaaf133898
jcavalcanti <josecavalcanti@Joses-MacBook-Pro.local>
2021-05-13 20:14:29 -0300

    First change

--- src/hilamatu/Main.java
+++ src/hilamatu/Main.java
@@ -1,8 +1,15 @@
 package hilamatu;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, my name is Moroni Hilamatu");
+        System.out.println("Hello Hilamatu, my name is Jose Cavalcanti");
+
+
     }
+
 }
