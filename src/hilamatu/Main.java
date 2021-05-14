6309a7e78bff6e07e7aef8e6839dc2c0adf0bef7
moroni1995 <u388764730@byui.edu>
2021-05-14 12:44:37 -0300

    Testing version control

--- src/hilamatu/Main.java
+++ src/hilamatu/Main.java
@@ -1,13 +1,12 @@
 package hilamatu;
 
-import java.sql.SQLOutput;
-
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello, my name is Moroni Hilamatu");
         System.out.println("Hello Hilamatu, my name is Jose Cavalcanti");
+        System.out.println("Hello Hilamatu and Jose, my name is Vinicius Alves");
 
 
     }
